f0c2a07b577c53d0d8d2cad0ed7cda5757c58174
JeongyoonLee <43804152+jeongyoonlee2015@users.noreply.github.com>
2019-08-02 10:24:03 +0900

    Update config.js

--- JsonConfig/config.js
+++ JsonConfig/config.js
@@ -1,196 +1,113 @@
+/* Magic Mirror Config Sample
+ *
+ * By Michael Teeuw http://michaelteeuw.nl
+ * MIT Licensed.
+ *
+ * For more information how you can configurate this file
+ * See https://github.com/MichMich/MagicMirror#configuration
+ *
+ */
+
 var config = {
-  address: "localhost",
-  port: 8080,
-  ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"],
-  language: "en",
-  timeFormat: 24,
-  units: "metric",
+	address: "localhost", // Address to listen on, can be:
+	                      // - "localhost", "127.0.0.1", "::1" to listen on loopback interface
+	                      // - another specific IPv4/6 to listen on a specific interface
+	                      // - "", "0.0.0.0", "::" to listen on any interface
+	                      // Default, when address config is left out, is "localhost"
+	port: 8080,
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
+	                                                       // or add a specific IPv4 of 192.168.1.5 :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
+	                                                       // or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
+	                                                       // ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
-  modules: [
-    {
-      module: "alert",
-    },
-    {
-      module: "updatenotification",
-      position: "top_bar"
-    },
-    {
-      module: "clock",
-      position: "top_left",
-      config: {
-        dateFormat: "LL dddd",
-      }
-    },
-    {
-      module: "calendar",
-      position: "top_left",
-      config: {
-        urgency: 21,
-        fetchInterval: 3600000,
-        calendars: [{
-          url: 'https://calendar.google.com/calendar/ical/2019captain11%40gmail.com/private-54ccbdf0a89e8f20a3dc94d991308ef5/basic.ics',
-          symbol: 'calendar-check',
-        }, ]
-      }
-    },
-    {
-      module: "currentweather",
-      position: "top_right",
-      config: {
-        location: "Seoul", //https://openweathermap.org/ 에서 지역을 찾으세요.
-        locationID: "",
-        appid: "24e8ec656f3ef8f436abeefc89490047" //openweathermap.org의 API key를 입력하세요.
-      }
-    },
-    {
-      module: "weatherforecast",
-      position: "top_right",
-      config: {
-        location: "Seoul", //https://openweathermap.org/ 에서 지역을 찾으세요.
-        locationID: "1835847",
-        appid: "24e8ec656f3ef8f436abeefc89490047" //openweathermap.org의 API key를 입력하세요.
-      }
-    },
-    {
-      module: "compliments",
-      position: "lower_third",
-      config: {
-        compliments: {
-          anytime: [ //아무때나
-            "CBNU X UKM SMAT"
-          ],
-          morning: [ //아침
-            "Good Morning Captain11",
-            "selamat pagi",
-            "Did you sleep well?"
-          ],
-          afternoon: [ //오후
-            "How are you captain?",
-            "selamat petang",
-            "Good afternoon"
-          ],
-          evening: [ //저녁
-            "How was your day today?",
-            "selamat malam",
-            "Good Evening"
-          ],
-          day_sunny: [//맑은 낮
-            "Sunny Time"
-          ],
-          day_cloudy: [//흐린 낮
-            "Cloudy Time"
-          ],
-          cloudy: [//흐림
-            "Today is Cloudy."
-          ],
-          cloudy_windy: [//흐리고 바람
-            "Cloudy n Windy"
-          ],
-          showers: [//소나기
-            "Showers Time"
-          ],
-          rain: [//비
-            "Rain droppp"
-          ],
-          thunderstorm: [//천둥번개
-            "Super Power Thunderstorm"
-          ],
-          snow: [//눈
-            "We make a Snowman"
-          ],
-          fog: [//안개
-            "foggy day"
-          ],
-          night_clear: [//맑은 밤
+	language: "en",
+	timeFormat: 24,
+	units: "metric",
 
-          ],
-          night_cloudy: [//흐린 밤
+	modules: [
+		{
+			module: "alert",
+		},
+		{
+			module: "updatenotification",
+			position: "top_bar"
+		},
+		{
+			module: "clock",
+			position: "top_left"
+		},
+		{
+			module: "calendar",
+			header: "SMAT",
+			position: "top_left",
+			config: {
+				calendars: [
+					{
+						symbol: "calendar-check",
+						url: "https://calendar.google.com/calendar/ical/2019captain11%40gmail.com/private-54ccbdf0a89e8f20a3dc94d991308ef5/basic.ics"
+					}
+				]
+			}
+		},
+		{
+			module: "compliments",
+			position: "lower_third",
+			config: {
+			  compliments: {
+				anytime:[
+					"Hello, Captain11!",
+					"UKM X CBNU SMAT"
+				],
+				morning:[
+					"Hello,Captain",
+					"Selamat pagi",
+					"Good morning Captain"
+				],
+				afternoon:[
+					  "Nice Captain",
+					  "Selamat petan"
+				]
+			  }
+			}
+		},
+		{
+			module: "currentweather",
+			position: "top_right",
+			config: {
+				location: "Seoul",
+				locationID: "",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "24e8ec656f3ef8f436abeefc89490047"
+			}
+		},		{
+			module: "weatherforecast",
+			position: "top_right",
+			header: "Weather Forecast",
+			config: {
+				location: "Seoul",
+				locationID: "1835847",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "24e8ec656f3ef8f436abeefc89490047"
+			}
+		},
+		{
+			module: "newsfeed",
+			position: "bottom_bar",
+			config: {
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
+			}
+		},
+	]
 
-          ],
-          night_showers: [//소나기 밤
-
-          ],
-          night_rain: [//비오는 밤
-
-          ],
-          night_thunderstorm: [//천둥번개 밤
-
-          ],
-          night_snow: [//눈오는 밤
-
-          ],
-          night_alt_cloudy_windy: [//흐리고 바람부는 밤
-
-          ],
-        }
-      }
-    },
-    {
-      module: "MMM-AssistantMk2",
-      position: "top_right",
-      config: {
-        record: {
-          recordProgram : "arecord",
-          device        : "plughw:1",
-        },
-        notifications: {
-          ASSISTANT_ACTIVATED: "HOTWORD_PAUSE",
-          ASSISTANT_DEACTIVATED: "HOTWORD_RESUME",
-        },
-        deviceLocation: {
-          coordinates: {
-            latitude: 37.57,
-            longitude: 126.98
-          }
-        },
-        profiles: {
-          "default": {
-           lang: "ko-KR"//"en-US"
-          }
-        }
-      }
-    },
-    {
-      module: "MMM-Hotword",
-      config: {
-        snowboy: [
-          {
-            hotwords: "smartmirror",
-            file: "resources/models/smart_mirror.umdl",
-            sensitivity: '1.0',
-          },
-          {
-            hotwords: "snowboy",
-            file: "resources/models/snowboy.umdl",
-            sensitivity: '1.0',
-          },
-          {
-            file: 'resources/models/jarvis.umdl',
-            sensitivity: '0.8,0.80',
-            hotwords: ['jarvis', 'jarvis']
-          }
-        ],
-        record: {
-          recordProgram : "arecord",
-          device        : "plughw:1",
-        },
-        autostart:true,
-        onDetected: {
-          notification: function (payload) {
-            return "ASSISTANT_ACTIVATE"
-          },
-          payload: function (payload){
-            return {
-              profile: payload.hotword
-            }
-          }
-        },
-      },
-    },
-  ]
 };
 
 /*************** DO NOT EDIT THE LINE BELOW ***************/
-if (typeof module !== "undefined") {
-  module.exports = config;
-}
+if (typeof module !== "undefined") {module.exports = config;}
